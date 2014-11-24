321d3eb8bc67f7539f70a5f26b1e9c56313dfe8b
Dirk Groenen <dirk@bitlabs.nl>
2014-11-24 22:27:02 +0100

    Made pip installable #4

---
+++ mopidy_mopify/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+import os
+
+from mopidy import config, ext
+
+__version__ = '0.1.4'
+
+class MopifyExtension(ext.Extension):
+    dist_name = 'Mopidy-Mopify'
+    ext_name = 'mopify'
+    version = __version__
+
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
+
+    def setup(self, registry):
+        registry.add('http:static', {
+            'name': self.ext_name,
+            'path': os.path.join(os.path.dirname(__file__), 'static'),
+        })   
---
+++ setup.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+import re
+from setuptools import setup, find_packages
+
+
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
+
+
+setup(
+    name='Mopidy-Mopify',
+    version=get_version('mopidy_mopify/__init__.py'),
+    url='https://github.com/dirkgroenen/Mopify',
+    license='GNU General Public License v3 (GPLv3)',
+    author='Dirk Groenen',
+    author_email='dirk@bitlabs.nl',
+    description='A Mopidy Web client based on the (old) Spotify interface. Improved to work with spotify as main library.',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Mopidy >= 0.19'
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'mopify = mopidy_mopify:MopifyExtension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+)
