dd706352c895c45e15af29e186320079c32f55f1
Dirk Groenen <dirk@taketwo.nl>
2014-12-22 15:51:17 +0100

    added topresult to search

--- mopidy_mopify/static/app/search/search.controller.js
+++ mopidy_mopify/static/app/search/search.controller.js
@@ -40,21 +40,32 @@ angular.module('mopify.search', [
      */
     $scope.performSearch = function performSearch(){
         var searchableItems = (!SpotifyLogin.connected) ? "album,artist" : "album,artist,playlist";
+        var resultsloaded = 0;
 
         Spotify.search($scope.query, searchableItems, {
             market: "NL",
             limit: "12"
         }).then(function(data){
+            resultsloaded++;
+
             $scope.results.artists = data.artists;
             $scope.results.albums = data.albums;
             $scope.results.playlists = data.playlists;
+
+            if(resultsloaded == 2)
+                $scope.topresult = getTopMatchingResult($scope.query, $scope.results).type;
         });
 
-
         mopidyservice.search($scope.query).then(function(data){
+            resultsloaded++;
+
             if(data[0].tracks !== undefined){
                 $scope.results.tracks = data[0].tracks.splice(0,15);
             }
+
+            // Check if all data is loaded and if it is; calculate the topresult
+            if(resultsloaded == 2)
+                $scope.topresult = getTopMatchingResult($scope.query, $scope.results);
         });
     };
 
@@ -65,6 +76,92 @@ angular.module('mopify.search', [
 
     if(mopidyservice.isConnected)
         $scope.performSearch();
+
+    /**
+     * Get the top matching resutls from the given batch
+     * @param  {string} search  The search string to check against
+     * @param  {object} results All the results from spotify and mopidy
+     * @return {object}         The item that came out as best match
+     */
+    function getTopMatchingResult(search, results){
+        var bestmatch = null;
+        var resultitem = {};
+        var items = [];
+        var results = angular.copy(results);
+
+        // Loop through all results and create an array with all items
+        _.each(results, function(result, key){
+            // Get correct items array
+            if(result.items){
+                items.push({
+                    type: key,
+                    items: result.items
+                });
+            }
+            else{
+                items.push({
+                    type: key,
+                    items: result
+                });
+            }
+        });
+
+        // Check each item with the query using the levenshtein algorithme
+        _.each(items, function(collection){
+            _.each(collection.items, function(item){
+                var distance = levenshteinDistance(search, item.name.toLowerCase());
+                
+                // Check with previous bestmatch and update if needed
+                if(bestmatch == null || bestmatch > distance){
+                    bestmatch = distance;
+                    resultitem = { item: item, type: collection.type };
+                }
+            });
+        });
+
+        return resultitem;
+    }
+
+    /**
+     * Compute the edit distance between the two given strings
+     * @param  {string} a 
+     * @param  {string} b 
+     * @return {int}   the number that represents the distance
+     */
+    function levenshteinDistance(a, b) {
+        if(a.length === 0) return b.length; 
+        if(b.length === 0) return a.length; 
+
+        var matrix = [];
+
+        // increment along the first column of each row
+        var i;
+        for(i = 0; i <= b.length; i++){
+            matrix[i] = [i];
+        }
+
+        // increment each column in the first row
+        var j;
+        for(j = 0; j <= a.length; j++){
+            matrix[0][j] = j;
+        }
+
+        // Fill in the rest of the matrix
+        for(i = 1; i <= b.length; i++){
+            for(j = 1; j <= a.length; j++){
+                if(b.charAt(i-1) == a.charAt(j-1)){
+                    matrix[i][j] = matrix[i-1][j-1];
+                } 
+                else {
+                    matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                                   Math.min(matrix[i][j-1] + 1, // insertion
+                                   matrix[i-1][j] + 1)); // deletion
+                }
+            }
+        }
+
+        return matrix[b.length][a.length];
+    };
 
 })
 
