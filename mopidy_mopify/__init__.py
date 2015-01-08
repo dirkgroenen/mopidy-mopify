b409f8dcdefce92c101abe9dc567dfe47d8f8b47
Dirk Groenen <dirk@bitlabs.nl>
2015-01-08 22:34:37 +0100

    moved settings to own branch

--- mopidy_mopify/__init__.py
+++ mopidy_mopify/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 
 import os
-import subprocess
-import tornado.web
 
 from mopidy import config, ext
 
@@ -23,28 +21,3 @@ class MopifyExtension(ext.Extension):
             'name': self.ext_name,
             'path': os.path.join(os.path.dirname(__file__), 'static')
         })
-
-        registry.add('http:app', {
-            'name': 'mopify-settings',
-            'factory': mopify_app_factory,
-        })
-
-
-class WebMopifySelfUpdateHandler(tornado.web.RequestHandler):
-    dist_name = __dist_name__
-
-    def initialize(self, core):
-        self.core = core
-
-    def post(self):
-        if not os.geteuid() == 0:
-            self.write('Mopidy must run as root if you want to automatically update Mopify')
-        else:
-            self.write('Updating Mopify...')
-            subprocess.call(['pip', 'install', '--upgrade', self.dist_name])    
-        
-
-def mopify_app_factory(config, core):
-    return [
-        ('/update', WebMopifySelfUpdateHandler, {'core': core})
-    ]
