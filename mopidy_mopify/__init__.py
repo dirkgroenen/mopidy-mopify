0a9581f1472ce68bd8f015dc5db166050b951cbb
Dirk Groenen <dirk@bitlabs.nl>
2015-01-13 22:16:47 +0100

    raised version and removed config code

--- mopidy_mopify/__init__.py
+++ mopidy_mopify/__init__.py
@@ -6,7 +6,7 @@ import tornado.web
 
 from mopidy import config, ext
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __ext_name__ = "mopify"
 
 class MopifyExtension(ext.Extension):
@@ -18,38 +18,8 @@ class MopifyExtension(ext.Extension):
         conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
         return config.read(conf_file)
 
-    def get_config_schema(self):
-        schema = super(MopifyExtension, self).get_config_schema()
-        schema['spotify_username'] = config.String('true')
-        schema['spotify_password'] = config.Secret('true')
-        schema['playlist_source'] = config.String('true', ['mopidy', 'spotify'])
-        schema['country_code'] = config.String('true')
-        schema['language_code'] = config.String('true')
-        return schema
-
     def setup(self, registry):
         registry.add('http:static', {
             'name': self.ext_name,
             'path': os.path.join(os.path.dirname(__file__), 'static'),
         })
-
-        registry.add('http:app', {
-            'name': self.ext_name + '-settings',
-            'factory': mopify_settings_factory,
-        })
-
-class RequestHandler(tornado.web.RequestHandler):
-    ext_name = __ext_name__
-
-    def initialize(self, core, config):
-        self.core = core
-        self.config_file = config.get(self.ext_name)
-
-    def get(self):
-        self.write("%s" % self.config_file)
-
-
-def mopify_settings_factory(config, core):
-    return [
-        ('/', RequestHandler, {'core': core, 'config': config})
-    ]
