5d79d894c3c68c4ed1d6f7c52064af5b51db0556
Dirk Groenen <dirk@bitlabs.nl>
2015-01-17 20:24:41 +0100

    Added remove action as mentioned in #26

--- src/app/widgets/station.directive.js
+++ src/app/widgets/station.directive.js
@@ -1,25 +1,45 @@
 'use strict';
 
 angular.module('mopify.widgets.directive.station', [
-    "mopify.services.station"
+    'mopify.services.station',
+    'LocalStorageModule'
 ])
 
-.directive('mopifyStation', function(stationservice) {
+.directive('mopifyStation', function(stationservice, localStorageService) {
 
     return {
         restrict: 'E',
         scope: {
-            station: '='
+            station: '=',
+            index: '='
         },
         templateUrl: 'widgets/station.directive.tmpl.html',
         link: function(scope, element, attrs) {
 
+            scope.visible = true;
+            
             /**
              * Start the provided station
-             * @param {station} station - station object containing all the information to start the new station
              */
-            scope.startStation = function(){
+            scope.start = function(){
                 stationservice.start(scope.station);
+            };
+
+            /**
+             * Delete the station from the user's storage
+             */
+            scope.delete = function(){
+                var stations = localStorageService.get("stations");
+
+                if(stations !== null){
+                    // Remove from storage
+                    stations.splice((stations.length - 1) - scope.index, 1);
+                    localStorageService.set("stations", stations);
+
+                    // Set visible false    
+                    scope.visible = false;
+                }
+                
             };
 
             scope.getStationUrl = function(){
